354b133a048b0615cc456fddd21cb0c090c665a6
Felipe-Camargo12 <felipeca1268@gmail.com>
2025-04-04 21:39:55 -0300

    feat: Criado paginação da lista usando pagination shadcn

---
+++ rick-morty-portal/src/components/paginationFooter/index.tsx
@@ -0,0 +1,86 @@
+"use client";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+type PaginationFooterProps = {
+  currentPage: number;
+  setCurrentPage: (page: number) => void;
+  totalPages: number;
+};
+
+export default function PaginationFooter({
+  currentPage,
+  setCurrentPage,
+  totalPages,
+}: PaginationFooterProps) {
+  // Função handle para navegação entre páginas
+  const handlePageChange = (page: number) => {
+    if (page > 0 && page <= totalPages) {
+      setCurrentPage(page);
+    }
+  };
+
+  return (
+    <Pagination>
+      <PaginationContent>
+        {/* Página Anterior */}
+        <PaginationItem>
+          <PaginationPrevious
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              handlePageChange(currentPage - 1);
+            }}
+          />
+        </PaginationItem>
+
+        {/* Páginas numeradas (de 3 em 3) */}
+        {[...Array(3)].map((_, i) => {
+          const pageNumber = currentPage + i - 1; // Ajusta as opçoes de páginas
+          if (pageNumber < 1 || pageNumber > totalPages) return null; // Ignora páginas fora do intervalo
+
+          return (
+            <PaginationItem key={pageNumber}>
+              <PaginationLink
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  handlePageChange(pageNumber);
+                }}
+                isActive={pageNumber === currentPage}
+              >
+                {pageNumber}
+              </PaginationLink>
+            </PaginationItem>
+          );
+        })}
+
+        {/* Ellipsis se houver mais páginas */}
+        {currentPage + 3 < totalPages && (
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+        )}
+
+        {/* Próxima Página */}
+        <PaginationItem>
+          <PaginationNext
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              handlePageChange(currentPage + 1);
+            }}
+            disabled={currentPage === totalPages} // Desabilitar se for a última página
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+}
